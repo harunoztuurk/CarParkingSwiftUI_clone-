b76a8d0ec00bbc9b8da311b2092f4ea52acd01c7
harunoztuurk <85230159+harunoztuurk@users.noreply.github.com>
2022-10-08 09:57:03 +0300

    Add files via upload

---
+++ CarParkingSwiftUI/CarParkingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  CarParkingSwiftUI_clone
+//
+//  Created by Harun Öztürk on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CarParkingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarParkingSwiftUI/ContentView.swift
@@ -0,0 +1,86 @@
+//
+//  ContentView.swift
+//  CarParkingSwiftUI_clone
+//
+//  Created by Harun Öztürk on 8.10.2022.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        FinalView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct FinalView: View {
+    @State var splashScreen  = true
+
+        var body: some View {
+             ZStack{
+                Group{
+                  if splashScreen {
+                      SplashScreen()
+                   }
+                  else{
+                      CarParkingView()
+                        }
+                    }
+                   .onAppear {
+                      DispatchQueue
+                           .main
+                           .asyncAfter(deadline:
+                            .now() + 3) {
+                       self.splashScreen = false
+                      }
+                    }
+                }
+            }
+        }
+
+
+struct SplashScreen: View {
+    var body: some View {
+        ZStack{
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            ZStack{
+                LottieView(filename: "car")
+                    .frame(width: 300, height: 300)
+                    .opacity(0.8)
+                
+                
+                
+                Text("C A R  P A R K I N G")
+                    .padding()
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .offset(x: 0, y: 70)
+                    
+            }
+        }
+    }
+}
+
+/////
